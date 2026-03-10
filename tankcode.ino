76335d2e2a9fa2b1121cfae404a6ecbb085992ee
Victor Casado <victor.casado.nyc@gmail.com>
2026-03-09 23:30:46 -0400

    code working at https://enes100.umd.edu/tools/simulator/ as well as I can get it without sensors. Imperfect because stuff like recursion and the sim having drift but that hopefully wont be present in otv

--- tankcode.ino
+++ tankcode.ino
@@ -1,3 +1,4 @@
+#include "Arduino.h"
 #include "Enes100.h" //libraries are included at the very top of the program
 #include "Tank.h"
 
@@ -29,6 +30,20 @@ void moveForward(int speed, int time){
   Tank.turnOffMotors();
 }
 
+void turnLeft(int speed, int time){
+  Tank.setRightMotorPWM(speed);
+  Tank.setLeftMotorPWM(-speed);
+  delay(time);
+  Tank.turnOffMotors();
+}
+
+void turnRight(int speed, int time){
+  Tank.setRightMotorPWM(-speed);
+  Tank.setLeftMotorPWM(speed);
+  delay(time);
+  Tank.turnOffMotors();
+}
+
 void debug(){
   Enes100.println("Connected: " + String(Enes100.isConnected()));
   Enes100.println("Visible: " + String(Enes100.isVisible()));
@@ -38,21 +53,100 @@ void debug(){
   Enes100.println("Ultrasonic Reading: " + String(Tank.readDistanceSensor(2)));
 }
 
+void turnTo(int direction){
+  float targetTheta;
+
+  if (direction == 0){
+    targetTheta = PI/2;
+  }
+    if (direction == 1){
+    targetTheta = 0;
+  }
+  if (direction == 2){
+    targetTheta = -PI/2;
+  }
+
+    while (Enes100.getTheta() < targetTheta - .02){
+  if (Enes100.getTheta() < targetTheta - 1){
+    turnLeft(100, 20);
+  }
+  
+    else if (Enes100.getTheta() < targetTheta - .3){
+    turnLeft(50, 10);
+  }
+  
+      else if (Enes100.getTheta() < targetTheta - .1){
+    turnLeft(20, 10);
+  }
+  
+    else if (Enes100.getTheta() < targetTheta - .02){
+    turnLeft(10, 1);
+  }}
+  
+      while (Enes100.getTheta() > targetTheta + .02){
+  if (Enes100.getTheta() > targetTheta + 1){
+    turnRight(100, 20);
+  }
+  
+    else if (Enes100.getTheta() > targetTheta + .3){
+    turnRight(50, 10);
+  }
+  
+      else if (Enes100.getTheta() < targetTheta + .1){
+    turnRight(20, 10);
+  }
+  
+    else if (Enes100.getTheta() > targetTheta + .02){
+    turnRight(10, 1);
+  }}
+  
+  if ((Enes100.getTheta() < targetTheta - .02 || Enes100.getTheta() > targetTheta + .02)){
+      turnTo(direction);
+  }
+
+}
+
 void setup() {
-  pinMode(PIN_US_FRONT_TRIG, OUTPUT);
-  pinMode(PIN_US_FRONT_ECHO, INPUT);
-//This function connects the tank to the vision system. The TX and RX pins of the tanks
-//are 52 and 50 respectively.
-Enes100.begin("FIRETEST", FIRE, 14, 1116, 52, 50);
+  //pinMode(PIN_US_FRONT_TRIG, OUTPUT);
+  //pinMode(PIN_US_FRONT_ECHO, INPUT);
+  //This function connects the tank to the vision system. The TX and RX pins of the tanks
+  //are 52 and 50 respectively.
+  Enes100.begin("FIRETEST", FIRE, 16, 1116, 52, 50);
+  Tank.begin();
 
 
-//print some text in Serial Monitor
-Enes100.println("Sucessfully connected to the Vision System");
+  //print some text in Serial Monitor
+  Enes100.println("Sucessfully connected to the Vision System");
+  
+    while(Enes100.getY() < 1.5){
+            turnTo(0);
+
+  moveForward(255, 50);}
+  while(Enes100.getX() < 1.8){
+        turnTo(1);
+
+  moveForward(255, 50);}
+  
+ while(Enes100.getY() > 0.5){
+    turnTo(2);
+    moveForward(255, 50);
+     
+ }
+
+while(Enes100.getX() < 3.8){
+        turnTo(1);
+
+    moveForward(255, 100);
+}
 }
 
 void loop() {
+  /*
   while (detectDistance(PIN_US_FRONT_TRIG, PIN_US_FRONT_ECHO) > 20){
     moveForward(255, 100);
   }
   delay(100);
+  */
+
+
 }
